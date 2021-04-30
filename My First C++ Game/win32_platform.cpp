bea2900dded2f1020b6fb01c5119db7dc7480be2
Anirudh242 <anirudhbhardwaj48@gmail.com>
2021-04-30 18:10:01 +0530

    Engine is done! Added input

--- My First C++ Game/win32_platform.cpp
+++ My First C++ Game/win32_platform.cpp
@@ -13,6 +13,8 @@ struct Render_State
 global_variable Render_State render_state;
 
 #include "renderer.cpp"
+#include "platform_common.cpp"
+#include "game.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -70,24 +72,71 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
     HWND window = CreateWindowA(window_class.lpszClassName, "First Game!", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
 	HDC hdc = GetDC(window);
 
+	Input input = {};
+
+	float delta_time = 0.016666f; // Delta time is the amount of time elapsed in one frame
+	LARGE_INTEGER frame_begin_time;
+	QueryPerformanceCounter(&frame_begin_time);
+
+	float performance_frequency;
+	{
+		LARGE_INTEGER perf;
+		QueryPerformanceFrequency(&perf);
+		performance_frequency = (float)(perf.QuadPart);
+	}
+
 	while (running) 
 	{
 		// Input
 		MSG message;
+
+		for (int i = 0; i < BUTTON_COUNT; i++)
+		{
+			input.buttons[i].changed = false;
+		}
+
 		while (PeekMessage(&message, window, 0, 0, PM_REMOVE))
 		{
-			TranslateMessage(&message);
-			DispatchMessage(&message);
+			switch (message.message)
+			{
+				case WM_KEYUP:
+				case WM_KEYDOWN:
+				{
+					u32 vk_code = (u32)message.wParam;
+					bool is_down = ((message.lParam & (1 << 31)) == 0);
+
+					#define process_button(b, vk)\
+					case(vk): {\
+						input.buttons[b].is_down = is_down;\
+						input.buttons[b].changed = true;\
+					} break;
+
+					switch (vk_code)
+					{
+						process_button(BUTTON_UP, VK_UP)
+						process_button(BUTTON_DOWN, VK_DOWN)
+						process_button(BUTTON_RIGHT, VK_RIGHT)
+						process_button(BUTTON_LEFT, VK_LEFT)
+					}
+				} break;
+
+				default:
+				{
+					TranslateMessage(&message);
+					DispatchMessage(&message);
+				}
+			}
 		}
 
 		// Simulate
-		clear_screen(0xff5500);
-		draw_rect(12, 0, 10, 10, 0x00ff22);
-		draw_rect(0, 8, 5, 5, 0x00ffa2);
-		draw_rect(8, 25, 20, 7, 0x2e608c);
-		draw_rect(8, 6, 8, 10, 0xf711f0);
+		simulate_game(&input, delta_time);
 
 		// Render
 		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmapinfo, DIB_RGB_COLORS, SRCCOPY);
+	
+		LARGE_INTEGER frame_end_time;
+		QueryPerformanceCounter(&frame_end_time);
+		delta_time = (float)(frame_end_time.QuadPart - frame_begin_time.QuadPart) / performance_frequency;
+		frame_begin_time = frame_end_time;
 	}
 }
